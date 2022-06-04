086212b56dbc0924d728f2ec0b4a24c4ed61825c
Bakhmai <begaev7@gmail.com>
2022-06-04 17:08:38 +0300

    Описание задачи:
    Необходимо ознакомиться с заготовкой и доработать приложение, которое взаимодействует с базой оперируя пользователем ( класс User ) и проверить свои методы заранее написанными JUnit тестами. По итогу все тесты должны быть пройдены. Разрешается посмотреть реализацию тестов.
    
    Для запуска теста необходимо найти класс в папке test ( показано в предыдущей лекции ) и при нажатии на него правой кнопкой мыши запустить, выбрав Run "Имя класса"
    
    Класс UserHibernateDaoImpl в рамках этой задачи не затрагивается (остаётся пустой)
    
    User представляет из себя сущность с полями:
    
    Long id
    String name
    String lastName
    Byte age
             Архитектура приложения создана с опорой на паттерн проектирования MVC ( частично, у нас не WEB приложение)

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,18 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("ADAM", "SMITH", (byte) 17);
+        userService.saveUser("JONES", "DOE", (byte) 44);
+        userService.saveUser("MARTIN", "VAN HARTEN", (byte) 64);
+        userService.saveUser("CLARK", "KENT", (byte) 33);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,54 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+
+    /*private static final String URL_KEY      = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";*/
+
+    private Connection conn;
+    public Util() {
+        try{
+            String URL = "jdbc:mysql://localhost:3306/mydb";
+            String USERNAME = "bakhman";
+            String PASSWORD = "bakhmai";
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connection OK!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection ERROR!!!");
+        }
+    }
+
+    /*public static Connection openConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connection OK!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection ERROR!!!");
+        }
+        return connection;
+    }*/
+
+    /*public static Connection openConnect() {
+        try{
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }*/
+
+    public Connection getConn() {
+        return conn;
+    }
+}
